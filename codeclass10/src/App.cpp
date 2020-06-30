6fa57565672108214e61132a9cdecf7489505cb5
Gabriel Reis Andrade <joaogabriel2@gmail.com>
2020-06-30 09:54:10 -0300

    atividade da aula 10
    adição do search

---
+++ codeclass10/src/App.cpp
@@ -0,0 +1,77 @@
+#include "App.h"
+
+#include <iostream>
+#include <string>
+
+App::App(const std::string& filename) : diary(filename)
+{
+}
+
+int App::run(int argc, char* argv[])
+{
+    if (argc == 1) {
+        return show_usage();
+    }
+
+    std::string action = argv[1];
+
+    if (action == "add") {
+        if (argc == 2) {
+            add();
+        } else {
+            add(argv[2]);
+        }
+    } else if (action == "list") {
+        list_messages();
+    } else if (action == "search") {
+        search(argv[2]);
+    } else {
+        return show_usage();
+    }
+
+    return 0;
+}
+
+void App::add()
+{
+    std::string message;
+    std::cout << "Enter your message:" << std::endl;
+    std::getline(std::cin, message);
+
+    add(message);
+}
+
+void App::add(const std::string message)
+{
+    diary.add(message);
+    diary.write();
+}
+
+void App::list_messages()
+{
+    for (size_t i = 0; i < diary.messages_size; ++i) {
+        const Message& message = diary.messages[i];
+        std::cout << "-" << message.content << std::endl;
+    }
+}
+
+void App::search(std::string what)
+{
+    Message* found_message;
+    found_message = diary.search(what);
+    if (found_message == nullptr)
+    {
+        std::cout << "No message found with " << what << std::endl;
+    }
+    else
+    {
+        std::cout <<"Message found: "<< found_message->content << std::endl;
+    }
+    
+
+}
+
+int App::show_usage()
+{
+    return 1;
+}
